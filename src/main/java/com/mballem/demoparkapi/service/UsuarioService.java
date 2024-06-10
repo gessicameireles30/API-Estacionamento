a2591e9172fc857c72925c64f301deabe718c262
gessicameireles30 <gessicadasilvameireles@gmail.com>
2024-06-10 16:17:15 -0300

    Adicionado DTO

--- src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
@@ -19,4 +19,31 @@ public class UsuarioService {
         return usuarioRepository.save(usuario);
 
     }
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id){
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado.")
+        );
+
+
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String senhaAtual, String novaSenha, String confirmaSenha) {
+        if(!novaSenha.equals(confirmaSenha)){
+            throw new RuntimeException("Nova senha não confere com confirmação de senha");
+        }
+        Usuario user = buscarPorId(id);
+        if(!user.getPassword().equals(senhaAtual)){
+            throw new RuntimeException("Sua senha não confere.");
+        }
+        user.setPassword(novaSenha);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+
+    }
 }
